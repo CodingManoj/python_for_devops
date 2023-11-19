54233dd0628c28d5b1544978bd6dbd339653e093
CodingManoj <thecloudcareers@gmail.com>
2023-11-19 16:33:51 -0500

    Key Words In Python

--- 004/002-calci.py
+++ 004/002-calci.py
@@ -2,7 +2,17 @@ a  = 100
 b  = 200 
 
 def addition():
-    sum(a+b)
+    sum = a+b 
     print("Value of a + b is :", sum)
 
+def substraction():
+    sub = b-a 
+    print("Value of b-a is :", sub)
+
+def multiplication():
+    mul = b * a 
+    print("Value of b*a is :", mul)
+
 addition()
+substraction()
+multiplication()
