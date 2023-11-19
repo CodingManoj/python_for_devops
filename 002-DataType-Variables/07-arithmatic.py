2e95a5f35a1bcb706d59437338ece58b5034b472
CodingManoj <thecloudcareers@gmail.com>
2023-11-19 13:59:13 -0500

    Python Push

---
+++ 002-DataType-Variables/07-arithmatic.py
@@ -0,0 +1,21 @@
+ # Python is a dynamic typing language, that also means you don't have to declare the type of variable.Based on the declared variable, it can understand the dataType of the variable
+
+val1 = 100.5
+val2 = 200
+
+add = val1 + val2 
+print("Sum Of Val1 & Val2:", add )
+
+sub = val2 - val1 
+print("Difference Of Val1 & Val2:", sub )
+
+mul = val2 * val1 
+print("Multiplication Of Val1 & Val2:", mul )
+
+
+div = val2 / val1 
+print("Val2 on division by val1:", div) 
+
+# Roudning with 3 digits 
+dev = round(10.248123424 ,3)
+print("Rounding of dev:", dev)
