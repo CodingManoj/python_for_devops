f051a6304c21c0b9ff503854ad73c76c933a7087
CodingManoj <thecloudcareers@gmail.com>
2023-11-19 13:08:23 -0500

    Python Push

--- 002-DataType-Variables/01-split.py
+++ 002-DataType-Variables/01-split.py
@@ -1,7 +1,12 @@
 ARN = "arn:aws:iam::123456789012:user/johndoe"
+PASSWD = "sshd:x:105:65534::/run/sshd:/usr/sbin/nologin"
+
+
 
 # Print useName only using inbuilt function
 
 print(ARN.split("/"))                       # gives the elements in a list 
-
 print(ARN.split("/")[1])                    # Printing 2nd element in the list
+
+# Printing UID
+print(PASSWD.split(':')[2])
