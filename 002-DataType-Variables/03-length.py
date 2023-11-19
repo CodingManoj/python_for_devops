5d79380294db02ecbbc4942759d59b913723f87a
CodingManoj <thecloudcareers@gmail.com>
2023-11-19 13:20:28 -0500

    Python Push

--- 002-DataType-Variables/03-length.py
+++ 002-DataType-Variables/03-length.py
@@ -1,2 +1,5 @@
 inspire = "Python For DevOps"
+# len is a default function to count the characters in a given string
 
+length = len(inspire)
+print("Length Of Inspire is :",  length)
---
+++ 002-DataType-Variables/04-replace.py
@@ -0,0 +1,9 @@
+inspire = "Python for Devops"
+
+inspire_caps = inspire.replace("devops" , "DevOps")
+inspire_totalCaps = inspire.upper()
+inspire_totalNoCaps = inspire.lower()
+
+print("Text After Replacememt :", inspire_caps)
+print("Text with upper case :", inspire_totalCaps)
+print("Text with lower case :", inspire_totalNoCaps)
