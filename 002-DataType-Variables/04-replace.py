b6338fd778eec8cdfcec6830f7a9811e77eca8bd
CodingManoj <thecloudcareers@gmail.com>
2023-11-19 13:21:53 -0500

    Python Push

--- 002-DataType-Variables/04-replace.py
+++ 002-DataType-Variables/04-replace.py
@@ -4,6 +4,10 @@ inspire_caps = inspire.replace("devops" , "DevOps")
 inspire_totalCaps = inspire.upper()
 inspire_totalNoCaps = inspire.lower()
 
+inspire_devops = inspire.split(" "[2])
+
 print("Text After Replacememt :", inspire_caps)
 print("Text with upper case :", inspire_totalCaps)
 print("Text with lower case :", inspire_totalNoCaps)
+print("Printing Core with split:", inspire_devops)
+
