54334535275989bd1cec2e987d225a339233758e
CodingManoj <thecloudcareers@gmail.com>
2023-11-19 11:30:08 -0500

    Python Push

--- 002-DataType-Variables/01-split.py
+++ 002-DataType-Variables/01-split.py
@@ -2,4 +2,14 @@ ARN = "arn:aws:iam::123456789012:user/johndoe"
 
 # Print useName only using inbuilt function
 
-print(ARN.split("/"))
+print(ARN.split("/"))                       # gives the elements in a list 
+
+print(ARN.split("/")[1])
+
+
+
+ProxyPass "/ajay"  "http://172.100.10.20/ajay"
+ProxyPassReverse "/ajay"  "http://172.100.10.20/ajay"
+
+ProxyPass "/prakash"  "http://172.100.10.20/prakash"
+ProxyPassReverse "/prakash"  "http://172.100.10.20/prakash"
