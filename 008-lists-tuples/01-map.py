46c81c15ff62fd382b4d46c42a129666975e2589
CodingManoj <thecloudcareers@gmail.com>
2023-11-20 15:19:02 -0500

    Python Push

--- 008-lists-tuples/01-map.py
+++ 008-lists-tuples/01-map.py
@@ -1,4 +1,5 @@
-names = ["mike","steve","john"]
-# names = ("mike","steve","john")
+# names = ["mike","steve","john"]
+names = ("mike","steve","john")
+names.append("christman")              # This append will only work for list, not for append.
 print(names)
 print("Pritnig tupe:", type(names))
