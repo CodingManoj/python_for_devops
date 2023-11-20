233e72976961f2cab65a45963a804825e74b022e
CodingManoj <thecloudcareers@gmail.com>
2023-11-19 19:50:51 -0500

    Python Push

--- 004_import/adv_calci.py
+++ 004_import/adv_calci.py
@@ -1,3 +1,7 @@
+''''
+
+Multie Line Comment In Python 
+
 a  = 100
 b  = 200 
 
@@ -16,3 +20,20 @@ def multiplication():
 addition()
 substraction()
 multiplication()
+
+This is not a great way as this don't have much modularity. The same can be done in a better way using this approach
+
+'''
+
+'''Always Keep In Mind : Function Should Take the Input : Perform the Task : Return the Output'''
+
+def addition(a,b):
+    sum = a+b 
+    return sum   
+
+def substraction(a,b):
+    sub = b-a 
+    return sub 
+
+print("Value Of a+b", addition(5,10))
+print("Value of b-a",substraction(100,200) )
