0c535e7ee159e9aef43fae586ae9707b4f4b3437
CodingManoj <thecloudcareers@gmail.com>
2023-11-20 21:52:36 -0500

    Python Push

--- 008-lists-tuples/01-map.py
+++ 008-lists-tuples/01-map.py
@@ -1,5 +1,7 @@
 # names = ["mike","steve","john"]
 names = ("mike","steve","john")
+print("Printing length:", len(names))
 names.append("christman")              # This append will only work for list, not for append.
 print(names)
-print("Pritnig tupe:", type(names))
+print("Printing type:", type(names))
+print("Printing length:", len(names))
