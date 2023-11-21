557549d8b996bfb02d9cf94422f21f4f13c8c679
CodingManoj <thecloudcareers@gmail.com>
2023-11-21 16:12:40 -0500

    Python Push

---
+++ 009-loops/01-while.py
@@ -0,0 +1,3 @@
+# While Loop Usage in DevOps Context
+
+# Delete the files till all the files in this xyz directory are deleted.
---
+++ 009-loops/02-break-continue.py
@@ -0,0 +1,29 @@
+# numbers = [1, 2, 3, 4, 5]
+# for number in numbers: 
+#         if number == 3
+#         break;
+#         print(number)
+
+# count = 0
+# while count < 6 :
+#         if count == 4:
+#                 break 
+#         print(count)
+#         count += 1
+
+# numbers = [1, 2, 3, 4, 5]
+# while number in numbers:
+#         if number == 3:
+#                 break 
+#         print(number)
+# numbers = [1,2,3,4,5]
+# for i in numbers: 
+#         if i == 3:
+#            break 
+#         print(i)
+
+numbers = [1,2,3,4,5]
+for i in numbers: 
+        if i == 3:
+           continue 
+        print(i)
