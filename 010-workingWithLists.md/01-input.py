9de8d290285582d515d95497f2142d1a8b3d247b
CodingManoj <thecloudcareers@gmail.com>
2023-11-21 17:20:21 -0500

    Python Push

--- 010-workingWithLists.md/01-input.py
+++ 010-workingWithLists.md/01-input.py
@@ -1,21 +1,20 @@
 import os 
-# name = input("Pls enter your name :")
-# print("Name of the user:", name)
 
 folders = input("Enter Directoy Names followed by spaces :").split()
 print("List of folders:", folders)
 
-# for folder in folders:
-#     print(folder)
 
-# ls in bash is equivalent to os.listdir() in python , listdir is a function in module. 
 for folder in folders:
-    files = os.listdir(folder) 
-    print("------Available files in the given folder ----- :", files)
-# printing files of a directory
+    try:
+        files = os.listdir(folder) 
+        print("------Available files in the given folder ----- :", files)
+    except: 
+        print("***** Ensure the directory name is value *****")
+
     for file in files:
-        print("*******: ", file)
+        print("*******:", file)
 
 # If you give an folder that's not there, it reports an error and that's not the expected behavior.
 # Exception Handling Matters a lot. Using try and catch we can do that in a very efficient way!
-    
+
+# ls in bash is equivalent to os.listdir() in python , listdir is a function in module. 
