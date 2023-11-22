17d62391085cb52179004ed43c2de2ef06f5f0bb
CodingManoj <thecloudcareers@gmail.com>
2023-11-22 16:40:46 -0500

    Python Push

--- 011-Dictionaries-Lists/02pr-list.py
+++ 011-Dictionaries-Lists/02pr-list.py
@@ -1,6 +1,7 @@
 import requests 
 
-response = requests.get("https://api.github.com/repos/instana/robot-shop/pulls")
+url="https://api.github.com/repos/instana/robot-shop/pulls"
+response = requests.get(url)
 # print(response)                             # status code 
 # print(response.json())                    # Prints Entire JSON
 # print(type(response.json()))              # Prints the class that's giving JSON
@@ -9,6 +10,13 @@ absolute_details = response.json()
 exit_code        = response 
 response_class   = type(response.json())
 
-print("Printint absolute_details:", absolute_details)
-print("Printing exit code :", exit_code)
-print("Reponse Class :", response_class)
+# print(absolute_details[0]["user"]["login"])     # This prints only one user., what if I want to know all the users ? Just run a loop
+# print("Printing exit code :", exit_code)
+# print("Reponse Class :", response_class)
+# print(len(absolute_details))                      # Prints the length of the json 
+
+for record in range(len(absolute_details)):
+    print(absolute_details[record]["user"]["login"]) 
+
+# Next use case, we also want to display the number of PR's that these users's has made
+
