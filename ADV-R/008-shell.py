911e382df563597245342c150f1196d8665f6996
CodingManoj <thecloudcareers@gmail.com>
2023-11-23 18:59:53 -0500

    Python Push

--- ADV-R/008-shell.py
+++ ADV-R/008-shell.py
@@ -2,8 +2,15 @@
 # https://docs.python.org/3/library/os.html#os.system 
 # os.system() is a function that can do all of OS Actions that's available in OS Module
 
+# import os 
+# command = "uptime" 
+# output = os.system(command)
+
+# print("System Update is:", output)
+
+
 import os 
-command = "uptime" 
+command = os.argv[1] 
 output = os.system(command)
 
-print("System Update is:", output)
+print("Output Of The Command", command, output)
