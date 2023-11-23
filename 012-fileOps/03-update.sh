14bfbea2976305f3c2c71ed95b11029b7981d36e
CodingManoj <thecloudcareers@gmail.com>
2023-11-23 13:26:36 -0500

    Python Push

--- 012-fileOps/03-update.sh
+++ 012-fileOps/03-update.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 CONNECTIONS=$1
-sed -e "/^MAX_CONNECTIONS/ c MAX_CONNECTIONS=$CONNECTIONS/" server.conf
+
+if [ "$CONNECTIONS" == "" ]; then 
+    echo -e "\e[31m Ensure you supply the value of connections in command line \n \t \t \e[0m"
+    echo -e "\t Ex: bash update.sh 1245"
+    exit 1
+fi 
+
+sed -e "/^MAX_CONNECTIONS/ c MAX_CONNECTIONS=$CONNECTIONS" server.conf
+
