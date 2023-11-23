8bf70341c66bcb5a9570b416c921d04dd9c4f649
CodingManoj <thecloudcareers@gmail.com>
2023-11-23 13:14:12 -0500

    B56 Auto Push

--- 012-fileOps/02-update.py
+++ 012-fileOps/02-update.py
@@ -1,13 +1,15 @@
 
-def udpate_conf(filepath, key, value): 
-    with open("filepath","r") as file: 
-        lines = file.readlines() 
-    
-    with open("filepath", "w") as file:
-        for line in lines: 
-            if key in line: 
-                file.write(key + "=" + value + "\n")
-            else:
-                file.write(line) 
 
-update_conf("server.conf", "MAX_CONNECTIONS" , "1200")
+# Declaring update conf functions 
+def update_conf(filepath, key, value): 
+    with open(filepath, 'r') as file:                   # Opening the file to read   # with is a system package and no need of any "pip install with"
+       lines = file.readlines()                         # Reading the file and storing in a variable in dictionary format
+    with open(filepath, 'w') as file:                   # Opening the file in write only mode
+        for line in lines:                              # Running the loop against the lines to find the supplied KeyWord
+            if key in line:                             # Replacing the Key Value if the key is present
+                file.write(key + "=" + value + "\n")
+            else:  
+                file.write(line)                        # We are just writing the same entry in case of the absense of key
+
+
+update_conf("server.conf", "MAX_CONNECTIONS", "1711")
