1edfc66674b3d1e6a4fa876759442f330e732beb
CodingManoj <thecloudcareers@gmail.com>
2023-11-23 21:10:32 -0500

    Python Push

--- ADV-R/010-tryException.py
+++ ADV-R/010-tryException.py
@@ -1,29 +1,10 @@
-import os 
 import sys
 
 print("Name of the script :", sys.argv[0])
 
 try:
-    def add(a,b):
-        sum = a + b 
-        return sum 
-except ValueError: 
-    print(" Input Error !!!!! Ensure both the supplied values are either int or float !!!!!!")    
-
-try:
-    def sub(a,b):
-        if a > b:
-            diff = a - b 
-            return diff 
-        else:
-            diff = b - a 
-            return diff 
-except ValueError: 
-    print(" Input Error !!!!! Ensure both the supplied values are either int or float !!!!!!")    
-
-
-a_val= int(sys.argv[1])
-b_val= int(sys.argv[2])
-
-print("Value of a+b is", add(a_val, b_val))
-print("Different of a & b is", sub(a_val, b_val))
+    value: int(sys.argv[1]) 
+    sum = int(sys.argv[1])  +  int(sys.argv[2]) 
+    print(sum)
+except:
+    print('Input As Not A Integer')
