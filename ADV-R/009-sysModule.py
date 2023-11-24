070eb2e2495d5bf7f369cc2290abf0668fe3a78c
CodingManoj <thecloudcareers@gmail.com>
2023-11-23 19:46:30 -0500

    Python Push

--- ADV-R/009-sysModule.py
+++ ADV-R/009-sysModule.py
@@ -25,3 +25,20 @@ b_val= float(sys.argv[2])
 
 print("Value of a+b is", add(a_val, b_val))
 print("Different of a & b is", sub(a_val, b_val))
+
+
+## Here if you supply integers or float, you get output, if you supply anything apart from that you'd get this out!!!
+# ((( Expected Output )))
+#  $ python 009-sysModule.py 10 23
+# Name of the script : 009-sysModule.py
+# Value of a+b is 33.0
+# Different of a & b is 13.0 
+
+# (((Unlikely Event)))
+
+# $ python 009-sysModule.py 10 a
+# Name of the script : 009-sysModule.py
+# Traceback (most recent call last):
+#   File "/workspaces/python_for_devops/RGV/009-sysModule.py", line 24, in <module>
+#     b_val= float(sys.argv[2])
+# ValueError: could not convert string to float: 'a'
