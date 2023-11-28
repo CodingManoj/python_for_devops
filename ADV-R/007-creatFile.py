c99c40ef83012f1fd0fea5f30df83fa2c2fd9c53
CodingManoj <thecloudcareers@gmail.com>
2023-11-28 14:59:43 -0500

    Python Script To Create Issue On Jira on the top of a mentioned project

--- ADV-R/007-creatFile.py
+++ ADV-R/007-creatFile.py
@@ -1,2 +1,16 @@
 # Google ---> Create file python3 using os module
 
+import os 
+
+name = os.getenv('HOME')
+print(name)
+
+with open("/tmp/py.py", "w") as file: 
+    file.write("This is a python file \n")
+    file.write("This file is written using python")'
+
+with open("/tmp/py.py", "r") as readfile:
+    filedata=readfile.read()
+
+print("Printing filedata", filedata)
+
