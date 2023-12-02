8e6330d0c8bb9fc51226ec582b51916e5c3da408
Manoj Pachigolla <manoj@MacBook-Pro.local>
2023-12-02 16:50:19 -0500

    Python Advanced - Sending Email

--- ADV-R/017-createEC2.py
+++ ADV-R/017-createEC2.py
@@ -1,13 +1,20 @@
+# Google ---> python boto3 aws 
+# for any for the aws resource cretion you need to connect to client here in our case it's ec2 
+
 import boto3
+import sys 
 
 ec2 = boto3.client('ec2')       # If you want to handle s3, replace ec2 with s3 in the client
-
+count=int(sys.argv[1])
 ec2.run_instances(
     ImageId='ami-0f75a13ad2e340a58',
     InstanceType='t3.micro',
     SecurityGroupIds=['sg-052fd946b7e11841a'],
+    MaxCount=count,
+    MinCount=count,
     TagSpecifications=[
         {
+            'ResourceType': 'instance',
             'Tags': [
                 {
                     'Key': 'Name',
@@ -17,4 +24,7 @@ ec2.run_instances(
         },
     ],
 )
+
+# Note, there won't be any state, we use python for infra provision with lamdda, where infra or app is immutable 
+# python 017-pythonBoto3 
 
