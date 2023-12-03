b71c1299021479128640eab82e7b05534ca8bd47
Manoj Pachigolla <manoj@MacBook-Pro.local>
2023-12-02 22:55:22 -0500

    Python Advanced - Creating Flask To Accept GitHub Commits

--- ADV-R/020-Flask/boto.py
+++ ADV-R/020-Flask/boto.py
@@ -1,10 +1,3 @@
-# Scenario : We want to fetch the information of our AWS Account Resources and display that.
-# And also I don't have access to AWS Console On Production Account as I am consultant and I don't have access, how can we do that ??? 
-# This can be achieved with python ( list of ec2, ecr, eks or anything of our choice ) and we want this info to be accessed by the other teammates using NGINX.
-
-# This creates an instance, fetches the list of VM's and displays us.
-# ec2 describe instances boto3
-
 import boto3
 
 ec2 = boto3.client('ec2', region_name='us-east-1')       # If you want to handle s3, replace ec2 with s3 in the client
