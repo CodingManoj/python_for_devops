498857293848f80c83dab5624be4d2dd8d0c8a0a
Manoj Pachigolla <manoj@MacBook-Pro.local>
2023-12-02 22:52:41 -0500

    Python Advanced - Creating Flask To Accept GitHub Commits

---
+++ ADV-R/020.realTimeUseCase.py
@@ -0,0 +1,11 @@
+# Scenario : We want to fetch the information of our AWS Account Resources and display that.
+# And also I don't have access to AWS Console On Production Account as I am consultant and I don't have access, how can we do that ??? 
+# This can be achieved with python ( list of ec2, ecr, eks or anything of our choice ) and we want this info to be accessed by the other teammates using NGINX.
+
+# First, let's serve the sample application using sample hello world.
+# Google ---> Python Hello World Web Program
+# ( Let's deploy an application on flast )
+
+# let's run a sample flask
+# pip3 install flask
+
