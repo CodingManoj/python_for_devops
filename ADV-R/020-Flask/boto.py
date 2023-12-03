0157decacd26220df0e65e5668ea8b96bd3a5538
Manoj Pachigolla <manoj@MacBook-Pro.local>
2023-12-02 23:18:54 -0500

    Python Advanced - Creating Flask To Accept GitHub Commits

--- ADV-R/020-Flask/boto.py
+++ ADV-R/020-Flask/boto.py
@@ -2,5 +2,5 @@ import boto3
 
 ec2 = boto3.client('ec2', region_name='us-east-1')       # If you want to handle s3, replace ec2 with s3 in the client
 
-op = ec2.describe_instances()                                 # We are not using any filters
-print(op)
+op = ec2.describe_instances()["Reservations"]["Instances"]                                # We are not using any filters
+print(op)           # This gives the data in json, for easy querying, convert this to a Dictionary
