8210e2a0a2a69cd3055bb85393b275facd6197fc
Manoj Pachigolla <manoj@MacBook-Pro.local>
2023-12-02 22:27:16 -0500

    Python Advanced - Creating Flask To Accept GitHub Commits

--- ADV-R/019-flaskWithFiles.py
+++ ADV-R/019-flaskWithFiles.py
@@ -2,7 +2,7 @@ from flask import Flask          # Just importing flask from the entire package
 app = Flask(__name__)            # To initiate flask : flask is just a frameWork for python
 
 app = Flask(__name__,
-            static_url_path='index.html', 
+            static_url_path='', 
             static_folder='./',)
 
 app.run('0.0.0.0', port=5001) 
