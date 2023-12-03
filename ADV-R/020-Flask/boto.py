16def00d791f76864e6e5997751d2ca11adced5b
Manoj Pachigolla <manoj@MacBook-Pro.local>
2023-12-03 12:51:16 -0500

    Python Advanced - Creating Flask To Accept GitHub Commits

--- ADV-R/020-Flask/boto.py
+++ ADV-R/020-Flask/boto.py
@@ -45,7 +45,7 @@ print(t)
 
 
 # Now let's send this output to a file so that it can be server over browser.
-with open('index.html', 'w') as file:
+with open('../index.html', 'w') as file:
     file.write(str(t))    # Mention the type as str, if not file write won't happen
 
 
