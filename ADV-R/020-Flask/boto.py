daee8ec6dc1e2ecc1169587502ed54a487f298a5
Manoj Pachigolla <manoj@MacBook-Pro.local>
2023-12-03 12:32:38 -0500

    Python Advanced - Creating Flask To Accept GitHub Commits

--- ADV-R/020-Flask/boto.py
+++ ADV-R/020-Flask/boto.py
@@ -19,6 +19,11 @@ out = ec2.describe_instances()["Reservations"]                     # This gives 
 # for instances in out: 
 #     for instance in instances["Instances"]:
 #         print("|", instance["InstanceId"], "|",  instance["PrivateIpAddress"], "|", instance["InstanceType"],  "|")
+# ref : https://stackoverflow.com/questions/9535954/printing-lists-as-tabular-data
+# | i-0191688df111a79de | 172.31.39.172 | t3.micro |
+# | i-047b2812fbff0245c | 172.31.32.218 | t3.micro |
+# | i-07650e27d53ec33b2 | 172.31.42.225 | t3.micro |
+
 
 t = PrettyTable(['InstanceID', 'PrivateIP', 'InstanceType'])
 for instances in out: 
@@ -26,11 +31,20 @@ for instances in out:
         t.add_row([instance["InstanceId"],instance["PrivateIpAddress"],instance["InstanceType"]])   
 print(t) 
 
-# ref : https://stackoverflow.com/questions/9535954/printing-lists-as-tabular-data
-# | i-0191688df111a79de | 172.31.39.172 | t3.micro |
-# | i-047b2812fbff0245c | 172.31.32.218 | t3.micro |
-# | i-07650e27d53ec33b2 | 172.31.42.225 | t3.micro |
+# +---------------------+---------------+--------------+
+# |      InstanceID     |   PrivateIP   | InstanceType |
+# +---------------------+---------------+--------------+
+# | i-0191688df111a79de | 172.31.39.172 |   t3.micro   |
+# | i-047b2812fbff0245c | 172.31.32.218 |   t3.micro   |
+# | i-07650e27d53ec33b2 | 172.31.42.225 |   t3.micro   |
+# +---------------------+---------------+--------------+
 
+# Now this information has to be accessed over internet.
 
 # But I want to add header to that ?
 # Google ---> Printing list as tabular data + Stackoverflow
+
+
+# Now let's send this output to a file so that it can be server over browser.
+with open('index.html', 'w') as file 
+    file.write("t")
