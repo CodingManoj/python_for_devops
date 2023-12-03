35d67cdbd91598673434ee7af4cc4b5335b8e5ea
Manoj Pachigolla <manoj@MacBook-Pro.local>
2023-12-03 00:17:17 -0500

    Python Advanced - Creating Flask To Accept GitHub Commits

--- ADV-R/020-Flask/boto.py
+++ ADV-R/020-Flask/boto.py
@@ -13,7 +13,8 @@ for instance in op:
     for IID in instance["Instances"]:
         instance_list.append([[IID["InstanceId"]],[IID["ImageId"]] ], tablefmt='orgtbl')
 
-print(tabulate(instance_list,tablefmt='orgtbl'))
+print(tabulate(instance_list, tablefmt='orgtbl'))
+
 # i-0191688df111a79de
 # i-047b2812fbff0245c
 # i-07650e27d53ec33b2
