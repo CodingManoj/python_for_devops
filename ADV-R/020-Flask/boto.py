4612a2b4c0137bed0285194e5794621b1546f6c0
Manoj Pachigolla <manoj@MacBook-Pro.local>
2023-12-02 23:31:02 -0500

    Python Advanced - Creating Flask To Accept GitHub Commits

--- ADV-R/020-Flask/boto.py
+++ ADV-R/020-Flask/boto.py
@@ -7,7 +7,11 @@ ec2 = boto3.client('ec2', region_name='us-east-1')       # If you want to handle
 # Info is a map, so we need to write a loop  ---> How to get data from a list json in python
 op = ec2.describe_instances()["Reservations"]  
 
-print(op)           # This gives the data in json, for easy querying, convert this to a Dictionary
-for item in op:
-    for instance in op["Instances"]:
-        print(instance)
+# print(op)           # This gives the data in json, for easy querying, convert this to a Dictionary
+# for item in op:
+#     for instance in op["Instances"]:
+#         print(instance)
+        
+for instance in reservation["Instances"]
+    ec2 = boto3.resource('ec2')
+    specificinstance = ec2.Instance(instance["InstanceId"])
